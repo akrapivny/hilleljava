aafa2e6996aceba675e656c8f77b08e6fc5af2c7
KRAB <krab1@mail.ru>
2015-04-03 21:00:26 +0300

    1 proga

---
+++ Hill/src/MyFirstClass.java
@@ -0,0 +1,29 @@
+/**
+ * Created by KRAB on 03.04.2015.
+ */
+public class MyFirstClass {
+    public static void main (String[] params) {
+        //коментарий
+
+       greelings();
+
+        double f=123;
+        double e=23;
+        double g=f/e;
+
+        m
+        System.out.println(g);
+
+    }
+
+    public static void greelings(){
+
+
+        String a="Hello world";
+        int b=5;
+
+        System.out.println(a+"  dfgdfgf" + b);
+
+
+    }
+}

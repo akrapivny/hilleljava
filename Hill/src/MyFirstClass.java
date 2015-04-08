e2a7b712aad423d1c8117104195b56f11fc954fa
KRAB <krab1@mail.ru>
2015-04-08 20:07:47 +0300

    1 proga

--- Hill/src/MyFirstClass.java
+++ Hill/src/MyFirstClass.java
@@ -1,33 +1,52 @@
-import javax.swing.*;
+import java.util.Scanner;
 
 public class MyFirstClass {
     public static void main (String[] params) {
 
+        double a=100;
+        double b=3;
 
-        String input = JOptionPane.showInputDialog("Input perimetr ") ;
-        double p = Double.parseDouble(input);
-        input = JOptionPane.showInputDialog("Input area ") ;
-        double s = Double.parseDouble(input);
-        double a;//сторона а
-        double b;//сторона b
-        double discriminant; //дискриминант
+        System.out.println(String.format("%g",a/b));
+     //   byte bb=(byte) 127;
+     //   int i=bb;
+      //  System.out.println(b);
 
-        String resultstring;
-        discriminant = Math.pow((p/2),2)-4*s;
+        int r=455;
+        int result = r^20;
+        System.out.println(result);
+        result=result^20;
+        System.out.println(result);
 
-        if (discriminant<0) {
-            resultstring = "No result";
-        } else if (discriminant>0) {
-            a=((p/2)+Math.sqrt(discriminant))/2;
-            b=p/2-a;
-            resultstring = "Rectangle sides: " + a + ", " + b;
-        } else  {
-            a=p/4;
-            b=p/4;
-            resultstring = "Rectangle sides: " + a + ", " + b;
+    //    Scanner scanner=new Scanner(System.in);//класс ввода с клавиатуры
+        //
+        // proverka
+      //  int vvod=scanner.nextInt();//ждет ввода инта
+
+
+        //case
+          int n=5;
+        switch (n){
+            case 1:
+                System.out.println("a");
+                break;
+            case 2:
+                System.out.println("b");
+                break;
+            case 3:
+                System.out.println("c");
+                break;
+            case 4:
+                System.out.println("d");
+                break;
+            case 5:
+                System.out.println("e");
+                break;
+            default:
+                System.out.println("fignya");
+
+
         }
-        JOptionPane.showMessageDialog(null,resultstring) ;
-        System. exit(0);
+
     }
 
 
--- Hill/src/SideFoundClass.java
+++ Hill/src/SideFoundClass.java
@@ -4,29 +4,31 @@ public class SideFoundClass {
 
     public static void main(String[] params) {
         String input = JOptionPane.showInputDialog("Input perimetr ");
-        double p = Integer.parseInt(input);
+        double p = Double.parseDouble(input);
         input = JOptionPane.showInputDialog("Input area ");
-        double s = Integer.parseInt(input);
-        double a;//сторона а
-        double b;//сторона b
-        double discriminant; //дискриминант
+        double s = Double.parseDouble(input);
+        double a;//side а
+        double b;//side b
+        double discriminant; //diskriminant
 
         discriminant = Math.pow((p / 2), 2) - 4 * s;
 
+        String resultstr;
+
         if (discriminant < 0) {
-            JOptionPane.showMessageDialog(null, "No result");
+            resultstr = "No result";
         } else if (discriminant > 0) {
             a = ((p / 2) + Math.sqrt(discriminant)) / 2;
             b = p / 2 - a;
-            JOptionPane.showMessageDialog(null, "Rectangle sides: " + a + ", " + b);
-        } else if (discriminant == 0) {
+//            resultstr = "Rectangle sides: " + a + ", " + b;
+            resultstr = "Rectangle sides: " + String.format("%g",a) + ", " + String.format("%g",b);
+        } else {
             a = p / 4;
             b = p / 4;
-            JOptionPane.showMessageDialog(null, "Rectangle sides: " + a + ", " + b);
-
+//            resultstr = "Rectangle sides: " + a + ", " + b;
+            resultstr = "Rectangle sides: " + String.format("%g",a) + ", " + String.format("%g",b);
         }
-
-        System.exit(0);
+        JOptionPane.showMessageDialog(null, resultstr);
     }
 }
 

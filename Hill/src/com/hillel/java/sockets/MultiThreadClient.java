a88e6dfe249055a71db1834a2207999c3bc834f6
akrapivny <krapivny.aleksey@gmail.com>
2015-06-22 19:21:06 +0300

    22062015

--- Hill/src/com/hillel/java/sockets/MultiThreadClient.java
+++ Hill/src/com/hillel/java/sockets/MultiThreadClient.java
@@ -1,7 +1,37 @@
 package com.hillel.java.sockets;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 /**
- * Created by AKRAPIVNY on 15.06.2015.
+ * Created by ITyan on 15.06.2015.
  */
 public class MultiThreadClient {
+
+    Socket socket;
+    PrintWriter writer;
+
+    public static void main(String[] args) throws IOException {
+        new MultiThreadClient().start();
+    }
+
+    public void start() throws IOException {
+        socket = new Socket("192.168.0.112", 3111);
+        writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String line = scanner.nextLine();
+            send(line);
+        }
+    }
+
+    public void send(String line) throws IOException {
+        writer.println(line);
+        writer.flush();
+    }
+
 }
--- Hill/src/com/hillel/java/sockets/MultiThreadServer.java
+++ Hill/src/com/hillel/java/sockets/MultiThreadServer.java
@@ -1,7 +1,31 @@
 package com.hillel.java.sockets;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * Created by AKRAPIVNY on 15.06.2015.
+ * Created by ITyan on 15.06.2015.
  */
 public class MultiThreadServer {
+
+    private Map<InetAddress, String> ipToName = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        new MultiThreadServer().start();
+    }
+
+    public void start() throws IOException {
+        ServerSocket serverSocket = new ServerSocket(3111);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            Thread thread = new Thread(new SockedThread(socket, ipToName));
+            thread.start();
+        }
+    }
 }
